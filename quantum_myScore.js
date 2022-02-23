c94712963f280bf223f050003e4e6b773c9bf6eb
mautest88 <98202088+mautest88@users.noreply.github.com>
2022-02-23 09:31:39 +0800

    update

---
+++ quantum_myScore.js
@@ -0,0 +1,11 @@
+﻿// 验证卡密
+
+const {
+    sendNotify, getUserInfo
+} = require('./quantum');
+
+
+!(async () => {
+    var user = await getUserInfo();
+    await sendNotify(`您的剩余积分：${user.MaxEnvCount}`);
+})();
