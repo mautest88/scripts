1e0da0c98746ce334163208d64870f05df174e2a
1111 <1111@qq.com>
2022-05-09 22:01:38 +0800

    1

--- ks_query.js
+++ ks_query.js
@@ -1,9 +1,8 @@
 /**
  * 该脚本作者来源量子用户：灰色头像@，QQ：97393412
  * 快手极速版 金币，现金查询
- * 用户变量：KSJSB
+ * 用户变量：ksjsbCookie
  * 值是:CK
- * 
  * */
 const {
     sendNotify, getEnvs, deleteEnvByIds
@@ -28,7 +27,7 @@ let user_id = process.env.user_id; //用户id
     for (var i = 0; i < cks.length; i++) {
         var ck = cks[i];
         var body = await ks_query(ck.Value);
-        if (body.result == 1 && result.data.userData) {
+        if (body.result == 1 && body.data.userData) {
             await sendNotify("金币：" + body.data.totalCoin + "\n" + "现金：" + body.data.totalCash + "\n" + "昵称：" + body.data.userData.nickname)
         } else {
             console.log(ck + "已失效，从量子中删除该CK");
