c731eacf512b1a6bf11f36440a2dd630e12e383a
hhh <hhh@qq.com>
2022-09-22 17:34:44 +0800

    1

---
+++ nyn.js
@@ -0,0 +1,22 @@
+﻿const got = require('got');
+const {
+    sendNotify
+} = require('./quantum');
+
+const api = got.extend({
+    retry: { limit: 0 },
+});
+
+!(async () => {
+    await api({
+        url: 'https://api.linhun.vip/api/Littlesistervideo?type=json',
+        headers: {
+            Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        },
+        method: 'get',
+    }).then(async response => {
+        var body = JSON.parse(response.body);
+        console.log(body.video)
+        await sendNotify({ msg: body.video, MessageType: 3 })
+    });
+})();
